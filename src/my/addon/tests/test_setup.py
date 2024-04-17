50ab67b89b969d81cd522836fee8e6d302af73ea
1letter <1letter@gmx.de>
2024-04-17 09:33:35 +0200

    format files

--- src/my/addon/tests/test_setup.py
+++ src/my/addon/tests/test_setup.py
@@ -1,5 +1,5 @@
-# -*- coding: utf-8 -*-
 """Setup tests for this package."""
+
 from my.addon.testing import MY_ADDON_INTEGRATION_TESTING  # noqa: E501
 from plone import api
 from plone.app.testing import setRoles
